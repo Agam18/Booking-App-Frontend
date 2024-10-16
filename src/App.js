ac5e00c28fa8ad90ce6c8628fabbde06b3e8a0ba
Agam <agam18oct@gmail.com>
2024-10-16 23:15:53 +0530

    kfsak

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Router,Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import MakeBookings from './components/MakeBookings';
 import ViewBookings from './components/ViewBookings';
 import Navbar from './components/Navbar';
@@ -10,15 +10,15 @@ function App() {
   return (
 
     <div>
-      <Router>
       <Routes>
+      
         <Route path='/login' element={<Login/>}/> 
         <Route path='/register' element={<Register/>}/>   
         <Route path='/' element={<LandingPage/>}/>
         <Route path="/make-bookings" element={<MakeBookings />} />
         <Route path="/view-bookings" element={<ViewBookings />} />
       </Routes>
-      </Router>
+      
       </div>
    
   );
